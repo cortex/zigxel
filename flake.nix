41b9ec03aa5d9cec6a50b16cf107311cacb41701
Joakim Lundborg <joakim.lundborg@gmail.com>
2025-06-08 12:57:42 +0200

    Add flake + direnv

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  description = "Zigxel";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    { nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = [
+            pkgs.zig
+            pkgs.picotool
+            pkgs.nil
+          ];
+        };
+      }
+    );
+}
